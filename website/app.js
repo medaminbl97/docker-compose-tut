28a18df07a9c53d56bd313bf20354543257e449d
medaminbl97 <102234444+medaminbl97@users.noreply.github.com>
2025-06-03 20:18:59 +0000

    delete all

--- website/app.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("changeColorBtn").addEventListener("click", function() {
-    // Random color
-    const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
-    document.body.style.backgroundColor = randomColor;
-});
